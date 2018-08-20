5b568ca908b7abc8cecb7a003a865cc7148e0d47
dbaettig <daniela.baettig@hotmail.com>
2018-08-20 15:01:31 +0200

    added delete function to admin page

--- restaurangbokning/php/deleteReservation.php
+++ restaurangbokning/php/deleteReservation.php
@@ -1,10 +1,14 @@
 <?
 require 'fetchDatabase.php';
 
+$formData = JSON_decode($_GET['formData']);
+echo($formData);
+
+$id = $formData;
+
 $statement = $pdo->prepare(
-    "DELETE FROM reservations WHERE id = :id") 
+    "DELETE FROM reservations WHERE id = '$id'"
 );
   
-$statement->execute(array(
-   ":id" => $_GET["id"];
-));
+$statement->execute(
+);
--- restaurangbokning/src/components/Admin.js
+++ restaurangbokning/src/components/Admin.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 
 class Admin extends Component {
     state = {
-        reservations: []
+        reservations: [],
+        reservationId: ""
     }
 
 
@@ -19,11 +20,30 @@ class Admin extends Component {
                 this.displayReservations(data);
             })
     }
+
+
     displayReservations = (data) => {
-        let reservations = data.map((reservation) => <li key={reservation.id}>{reservation.date}{reservation.time}{reservation.participants}
-        </li>);
+        let reservations = data.map((reservation) => <li key={reservation.id}>{reservation.date}{reservation.time}
+            {reservation.participants}
+            <button name={reservation.id} onClick={this.deleteReservation}>Delete</button></li>);
+
         this.setState({ reservations: reservations })
-        console.log(reservations)
+    }
+
+    deleteReservation = (event) => {
+        console.log(event.target);
+
+        fetch('http://localhost:8888/deleteReservation.php?formData=' + event.target.name, {
+            method: 'GET',
+            headers:
+            {
+                'Accept': 'application/json',
+                'Content-type': 'application/json',
+            }
+        })
+            .then((response) => {
+                console.log(response);
+            })
 
     }
 
@@ -32,6 +52,7 @@ class Admin extends Component {
         return (
             <div>
                 <ul>{this.state.reservations}</ul>
+
             </div>
 
         );
