a1b214762ccfd09464791ce880671de11b140ee7
dbaettig <daniela.baettig@hotmail.com>
2018-08-20 21:44:21 +0200

    added som css to beable to work better and added a page reloader when deleteing a booking

---
+++ restaurangbokning/src/components/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class Header extends Component {
+
+    render() {
+        return (
+
+            <div>
+                <header><h2>BOKA BORD</h2>
+                </header>
+            </div>
+        )
+
+    }
+}
+
+export default Header;
