0fdc46a410e823d46f38f8fdf6a46a356daeba84
Loka Kanarp <lokakanarp@gmail.com>
2018-08-21 12:39:46 +0200

    changed fetchReservations and now working on admin form

--- restaurangbokning/php/fetchReservations.php
+++ restaurangbokning/php/fetchReservations.php
@@ -1,7 +1,9 @@
 <?php
 require 'fetchDatabase.php';
 
-$statement = $pdo -> prepare("SELECT * FROM reservations");
+$statement = $pdo -> prepare("SELECT * FROM reservations
+INNER JOIN guest
+ON reservations.guestId = guest.id");
 $statement -> execute();
 $reservations = $statement -> fetchAll(PDO::FETCH_ASSOC);
 
--- restaurangbokning/src/components/Admin.js
+++ restaurangbokning/src/components/Admin.js
@@ -3,7 +3,8 @@ import React, { Component } from 'react';
 class Admin extends Component {
     state = {
         reservations: [],
-        reservationId: ""
+        reservationId: "",
+		changeReservationForm: false
     }
 
 
@@ -18,16 +19,37 @@ class Admin extends Component {
                 console.log(data);
                 this.displayReservations(data);
             })
+		/*const urls = [
+   'http://localhost:8888/fetchReservations.php',
+   'http://localhost:8888/fetchGuest.php'
+  ];
+
+  // use map() to perform a fetch and handle the response for each url
+  Promise.all(urls.map(url =>
+   fetch(url)
+   .then((response) => response.json())
+  ))
+  .then(data => {
+   // do something with the data
+   console.log(data)
+  })*/
+
     }
 
 
     displayReservations = (data) => {
-        let reservations = data.map((reservation) => <li key={reservation.id}> {reservation.date}{' '}{reservation.time}{' '}
-            {reservation.participants}
-            <button name={reservation.id} onClick={this.deleteReservation}>Delete</button></li>);
+        let reservations = data.map((reservation) => <div key={reservation.id}> Datum: {reservation.date} {reservation.time}
+            {reservation.participants} {reservation.firstName}
+            <button name={reservation.id} onClick={this.deleteReservation}>Delete</button>
+			<button name={reservation.id} onClick={this.changeReservation}>Change</button></div>);
 
         this.setState({ reservations: reservations })
     }
+	
+	changeReservation = (event) => {
+		this.setState({reservationId: event.target.name});
+		
+	}
 
     deleteReservation = (event) => {
         console.log(event.target);
@@ -45,13 +67,22 @@ class Admin extends Component {
     }
 
     render() {
-
+		let styling = 'hidden';
+		{this.state.changeReservationForm ? (styling += "display") : (styling = 'hidden')}
         return (
+			<div>
+			<form method="POST" className={styling}>
+				<input type="number" min="1" max="6" name="participants" placeholder="2 People" onChange={this.handleChange} />
+				<input type="date" name="date" onChange={this.handleChange} />
+				<button type="submit" value="submit">BOOK</button>
+			</form>
+			
             <div className="displayBookings">
 
-                <ul>{this.state.reservations}</ul>
+                {this.state.reservations}
 
             </div>
+			</div>
 
         );
     }
