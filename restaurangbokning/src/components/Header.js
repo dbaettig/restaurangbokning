b6535e4199712102a24e1385eacadb1f113247bc
cami <evacamilla.s@gmail.com>
2018-08-29 09:34:49 +0200

    Add style if main navigation link is active

--- restaurangbokning/src/components/Header.js
+++ restaurangbokning/src/components/Header.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import '../App.css';
 import {
-  Link
+  NavLink
 } from 'react-router-dom';
 import ReactLoading from 'react-loading';
 
@@ -14,18 +14,17 @@ class Header extends Component {
             <div>
                 <div>
 					<ul>
-					  <li> <Link to="/admin">ADMIN</Link> </li>
-					  <li> <Link to="/">GUEST</Link> </li>
+                        <li> <NavLink activeClassName="linkIsActive" exact={true} to="/">GUEST</NavLink> </li>
+                        <li> <NavLink activeClassName="linkIsActive" to="/admin">ADMIN</NavLink> </li>
 					</ul>
 				</div>
-                <header>
-					<h2>BOKA BORD</h2>
-                </header>          
-                
-            </div>
-            
-        )
 
+                <header>
+                    <h2>BOKA BORD</h2>
+                </header>   
+            </div>
+
+        )
     }
 }
 
