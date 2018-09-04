95f65be0f414c10cd34cb91f511b99f70b4a548e
dbaettig <daniela.baettig@hotmail.com>
2018-09-04 14:29:17 +0200

    footer changes

--- restaurangbokning/src/components/ChangeGuestForm.js
+++ restaurangbokning/src/components/ChangeGuestForm.js
@@ -2,18 +2,20 @@ import React, { Component } from 'react';
 import '../App.css';
 
 class ChangeGuestForm extends Component {
-	
+
     render() {
         return (
-            <div>
-                <form>
-                    <input type="text" name="firstName" value={this.props.state.firstName} onChange={this.props.handleChange} />
-					<input type="text" name="lastName" value={this.props.state.lastName} onChange={this.props.handleChange} />
-					<input type="text" name="phone" value={this.props.state.phone} onChange={this.props.handleChange} />
-					<input type="text" name="email" value={this.props.state.email} onChange={this.props.handleChange} />
-                    <button type="submit" value="submit" onClick={this.props.changeGuest}>Change</button>
-                    <button onClick={this.props.closeChangeGuestForm}>Cancel</button>
-                </form>
+            <div className="wrapper">
+                <div className="formWrapper">
+                    <form>
+                        <input type="text" name="firstName" value={this.props.state.firstName} onChange={this.props.handleChange} />
+                        <input type="text" name="lastName" value={this.props.state.lastName} onChange={this.props.handleChange} />
+                        <input type="text" name="phone" value={this.props.state.phone} onChange={this.props.handleChange} />
+                        <input type="text" name="email" value={this.props.state.email} onChange={this.props.handleChange} />
+                        <button type="submit" value="submit" onClick={this.props.changeGuest}>Change</button>
+                        <button onClick={this.props.closeChangeGuestForm}>Cancel</button>
+                    </form>
+                </div>
             </div>
         )
     }
--- restaurangbokning/src/components/ChangeReservationForm.js
+++ restaurangbokning/src/components/ChangeReservationForm.js
@@ -2,47 +2,55 @@ import React, { Component } from 'react';
 import '../App.css';
 
 class ChangeReservationForm extends Component {
-	
-    render() {
+
+	render() {
 		let buttonStyle = 'hidden';
 		this.props.appState.buttonStyle ? buttonStyle = 'display' : buttonStyle = 'hidden';
-		let today = new Date().toJSON().slice(0,10);
+		let today = new Date().toJSON().slice(0, 10);
 		console.log(this.props.appState.date);
-        return (
-            
-			<div className="formWrapper">
-				{this.props.appState.showButtonForChangeRes ? (
-					<div>		
-						<p>Change the reservation to {this.props.appState.participants} people at {this.props.appState.chosenSitting} o'clock on {this.props.appState.date}.</p>
-						<button onClick={this.props.changeReservation}>Change</button>
-					</div>
-				) : (
-					<div>
-						<form method="POST" className="dateForm" onSubmit={this.props.fetchDate}>
-							<input type="number" min="1" max="6" name="participants" placeholder={this.props.appState.participants} onChange={this.props.handleChange} />
-							<input type="date" required min={today} placeholder={this.props.appState.date} name="date" onChange={this.props.handleChange} />
-							<button type="submit" value="submit">SEARCH AVAILABILITY</button>
-						</form>
+		return (
+			<div className="wrapper">
 
-						<div className={"timeButtons " + buttonStyle}>
-							<p>Available sittings:</p>
-							{this.props.appState.firstSitting ? (
-								<button name="chosenSitting" value="06:00" onClick={(event) => { this.props.handleChange(event); this.props.showButtonForChangeRes(); }}>
-									06:00</button>
-							) : (<p>There are no available tables at 06:00 PM.</p>)
-							}
-							{this.props.appState.secondSitting ? (
-								<button name="chosenSitting" value="09:00" onClick={(event) => { this.props.handleChange(event); this.props.showButtonForChangeRes(); }}>
-									09:00</button>
-							) :
-								(<p>There are no available tables at 09:00 PM.</p>)
-							}
+				<div className="formWrapper">
+					{this.props.appState.showButtonForChangeRes ? (
+						<div>
+							<p>Change the reservation to {this.props.appState.participants} people at {this.props.appState.chosenSitting} o'clock on {this.props.appState.date}.</p>
+							<button onClick={this.props.changeReservation}>Change</button>
 						</div>
-					</div>
-					)}
-			</div>	
-        )
-    }
+					) : (
+							<div>
+								<p>
+									Your previous reservation:<br />
+									{this.props.appState.participants} people
+									for {this.props.appState.date} at {this.props.appState.chosenSitting} pm.
+								</p>
+								<p>Change reservation:</p>
+								<form method="POST" className="dateForm" onSubmit={this.props.fetchDate}>
+									<input type="number" min="1" max="6" name="participants" placeholder={this.props.appState.participants} onChange={this.props.handleChange} />
+									<input type="date" required min={today} placeholder={this.props.appState.date} name="date" onChange={this.props.handleChange} />
+									<button type="submit" value="submit">SEARCH AVAILABILITY</button>
+								</form>
+
+								<div className={"timeButtons " + buttonStyle}>
+									<p>Available sittings:</p>
+									{this.props.appState.firstSitting ? (
+										<button name="chosenSitting" value="06:00" onClick={(event) => { this.props.handleChange(event); this.props.showButtonForChangeRes(); }}>
+											06:00</button>
+									) : (<p>There are no available tables at 06:00 PM.</p>)
+									}
+									{this.props.appState.secondSitting ? (
+										<button name="chosenSitting" value="09:00" onClick={(event) => { this.props.handleChange(event); this.props.showButtonForChangeRes(); }}>
+											09:00</button>
+									) :
+										(<p>There are no available tables at 09:00 PM.</p>)
+									}
+								</div>
+							</div>
+						)}
+				</div>
+			</div>
+		)
+	}
 }
 
 export default ChangeReservationForm;
